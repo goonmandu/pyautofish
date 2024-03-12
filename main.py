8ebadec4ef0049a7b9adee53a625c0ba8ae96ca6
Mingi Hwang <61984863+goonmandu@users.noreply.github.com>
2024-03-11 21:58:42 -0400

    Time formatting and finetune values

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import keyboard
 import win32con
 import win32api
 from window_image import snapshot
-print("Done.")
 
 mouse = Controller()
 avg_time = 0.0
@@ -14,17 +13,25 @@ catch_time = 0.0
 time_to_catch = 0.0
 total_time = 0.0
 catch_number = 0
-time_to_catch_string = ""
-avg_time_string = ""
-catch_number_string = ""
-exit_key = "ctrl"
+exit_key = "f7"
 mouse_move_dir = True
+
 
 def right_click():
     mouse.press(Button.right)
     mouse.release(Button.right)
 
+
+def format_number(number, decimal_digits):
+    if number < 10:
+        return f" {round(number, decimal_digits)}"
+    else:
+        return f"{round(number, decimal_digits)}"
+
+
 hundredpct_coords = ((178, 261), (288, 261), (398, 261))
+print("Done.")
+
 mcidx = int(input("Where is Minecraft located in the Volume Mixer? Enter 1-3. ")) - 1
 move_mouse = input("Bypass McMMO's overfishing anti-autofisher? [Y]es/[Any]No: ").lower() in ["yes", "y"]
 
@@ -34,13 +41,15 @@ print(f"Minecraft location in VMix: {mcidx+1}")
 print(f"McMMO Overfishing Bypass: {move_mouse}")
 input("\nPress [Enter] to confirm these settings and start the autofisher. If not, press Ctrl+C to exit.")
 
-print("Starting in 5 seconds.")
-
+print("Starting in 5 seconds.\n")
 time.sleep(5)
+print("---- Autofish Statistics ----")
 
 right_click()
+
 throw_time = time.time()
-time.sleep(1)
+time.sleep(3)
+
 while True:
     if keyboard.is_pressed(exit_key):
         raise Exception(f"Terminated by user: pressed {exit_key.upper()}")
@@ -52,9 +61,10 @@ while True:
         time_to_catch = catch_time - throw_time                 # end timer and save Δtime.time()
         total_time += time_to_catch                             # update total time spent fishing
         avg_time = total_time / catch_number                    # update average time per fish caught
-        print("Catch " + str(catch_number) + ": " + str(round(time_to_catch, 2)) + " | AVG: " + str(round(avg_time, 2)))
-                                                                # print fishing stats TODO: Formatting
-        time.sleep(0.75)                                        # time delay between catch and recast rod
+        print(f"Catch {catch_number}: {format_number(time_to_catch,1)}s "
+              f"| Avg: {format_number(avg_time, 1)}s")
+                                                                # print fishing stats
+        time.sleep(1)                                           # time delay between catch and recast rod
         if move_mouse:
             if mouse_move_dir:
                 for i in range(160):
@@ -66,4 +76,4 @@ while True:
                 mouse_move_dir = True
         right_click()                                           # cast fishing rod
         throw_time = time.time()                                # start timer
-        time.sleep(2)                                           # wait for volume meter to die down
+        time.sleep(3)                                           # wait for volume meter to die down
